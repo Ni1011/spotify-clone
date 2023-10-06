c2a1179156b32eb425dae3efc1dec05ae50ba555
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2023-10-06 14:39:11 +0530

    player slider

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,6 +4,7 @@ import useGetSongById from '@/hooks/useGetSongById'
 import useLoadSongUrl from '@/hooks/useLoadSongUrl'
 import usePlayer from '@/hooks/usePlayer'
 import React from 'react'
+import PlayerContent from './PlayerContent'
 
 const Player = () => {
     const player = usePlayer()
@@ -17,7 +18,9 @@ const Player = () => {
 
     
   return (
-    <div className=' fixed bottom-0 bg-black w-full py-2 h-[80px] px-4 '>Player</div>
+    <div className=' fixed bottom-0 bg-black w-full py-2 h-[80px] px-4 '>
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
+    </div>
   )
 }
 
---
+++ components/Slider.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import * as RadixSlider from '@radix-ui/react-slider'
+
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div>Slider</div>
+  )
+}
+
+export default Slider
